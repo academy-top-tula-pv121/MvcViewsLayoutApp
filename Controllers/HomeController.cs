9e85e84b4787406eb298c305be13aca870c6a551
maxefimov <efimov.top.academy@gmail.com>
2023-03-02 21:16:37 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcViewsLayoutApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Works()
+        {
+            return PartialView();
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,14 @@
+﻿namespace MvcViewsLayoutApp
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int Age { set; get; }
+        public User() { }
+        public User(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
